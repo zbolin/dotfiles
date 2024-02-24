51f952444bc52f24c6e0a62b168f39b66cd12476
Zach Bolin <zdbolin@gmail.com>
2024-02-24 12:49:24 -0600

    update run_once_install_ansible.sh

--- run_once_install_ansible.sh
+++ run_once_install_ansible.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-install_on_fedora() {
-    sudo dnf install -y ansible
-}
-
 install_on_ubuntu() {
     sudo apt-get update
     sudo apt-get install -y ansible
@@ -16,9 +12,7 @@ install_on_mac() {
 OS="$(uname -s)"
 case "${OS}" in
     Linux*)
-        if [ -f /etc/fedora-release ]; then
-            install_on_fedora
-        elif [ -f /etc/lsb-release ]; then
+        if [ -f /etc/lsb-release ]; then
             install_on_ubuntu
         else
             echo "Unsupported Linux distribution"
