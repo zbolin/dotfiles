447cdaf9f9d8267b6e49189f813d6c963635b674
Zach Bolin <zdbolin@gmail.com>
2024-02-24 12:40:08 -0600

    update run_once_install_ansible.sh

--- run_once_install_ansible.sh
+++ run_once_install_ansible.sh
@@ -1,3 +1,34 @@
-#!/bin/bash
+#~/bin/bash
 
-echo "Ansible script finished!"
+install_on_ubuntu() {
+	sudo apt-get update
+	sudo apt-get install -y ansible
+}
+
+install_on_mac() {
+	brew install ansible
+}
+
+OS="$(uname -s)"
+case "${OS}" in
+	Linux*)
+		if [ -f /etc/lsb-release ]; then
+			install_on_ubuntu
+		else
+			echo "Unsupported Linux distribution"
+		exit 1
+		fi
+		;;
+	Darwin*)
+		install_on_mac
+		;;
+	*)
+		echo "Unsupported operating system: ${OS}"
+		exit 1
+		;;
+esac
+
+
+#ansible-playbook ~/.bootstrap/setup.yml --ask-become-pass
+
+echo "Ansible installation complete."
