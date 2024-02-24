faf6c00a89200e7fb46a5e585f420c25bdb813cc
Zach Bolin <zdbolin@gmail.com>
2024-02-24 14:51:52 -0600

    remove some stuff

--- dot_config/zsh/config.d/tomcat.zsh
+++ dot_config/zsh/config.d/tomcat.zsh
@@ -1,7 +1,3 @@
-# brew services start tomcat@8
-# brew servcies stop tomcat@8
-# find where tomcat is:
-# brew ls tomcat @8 
 tomcatusers ()  {
     nvim '/opt/homebrew/Cellar/tomcat@8/8.5.85/libexec/conf/tomcat-users.xml'
 }
